7248623c6c77dceb49f91726508db9795e92f40f
birkhoff <mike.birkhoff@gmx.de>
2012-08-01 16:57:49 +0200

    Killa Init

--- src/gui/Gui.java
+++ src/gui/Gui.java
@@ -1,5 +1,26 @@
 package gui;
 
-public class Gui {
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Gui extends JFrame{
+	
+	
+	public Gui(){
+		setSize(300,400);
+		setVisible(true);
+		setResizable(false);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setLocationRelativeTo(null);
+		setTitle("Coque");
+		add( new GamePanel());
+	}
+	
+	public static void main(String[] args){
+		
+		Gui guiGame = new Gui();
+	}
 
 }
