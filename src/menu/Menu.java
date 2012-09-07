bed8f3f5ed8c74361fdec565fa008d1a666f1eb1
n2o <cmeter@googlemail.com>
2012-09-07 18:14:59 +0200

    Eigene Menüklasse für verschiedene Menüs

--- src/menu/Menu.java
+++ src/menu/Menu.java
@@ -1,38 +1,7 @@
-/**
- * Hier alle moeglichen Arten von Menues wie Hauptmenue, Pausemenue, ... erstellen
- */
-
 package menu;
 
-import java.awt.Color;
-import java.awt.Graphics2D;
-
 public class Menu {
-	
-	public static void showMainMenu(Graphics2D g, int selection, int mWidth, int mHeight) {
-		int heightPos = 1; 
-		int start = 100;
-		g.setColor(Color.black);
-		g.drawString("Hauptmenue", mWidth/2-30, 100);
-		Option option[] = new Option[5];
-		option[0] = new Option("Spiel starten");
-		option[1] = new Option("Spielstand laden");
-		option[2] = new Option("Einstellungen");
-		option[3] = new Option("Die ultimativen Entwickler");
-		option[4] = new Option("Einstellungen");
-		for (int i = 0; i < option.length; i++) {
-			if (selection == i) {
-				g.setColor(Color.red);
-				g.drawString(option[i].getText(), mWidth/2-30, start+30*heightPos);
-				System.out.println("Schreibe "+option[i].getText());
-				heightPos++;
-			} else {
-				g.setColor(Color.black);
-				g.drawString(option[i].getText(), mWidth/2-30, start+30*heightPos);
-				System.out.println("Schreibe "+option[i].getText());
-				heightPos++;
-			}
-		}
-		
+	public static void main() {
+		// Do nothing and destroy the world!
 	}
 }
