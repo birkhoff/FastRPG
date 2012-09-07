28b6bcf0b6121a5d0659b88c4aaa8422f461d661
Alexander Schneider <wegi.pwnz@googlemail.com>
2012-09-07 13:59:10 +0200

    Added XMLReader class

---
+++ src/engine/Map.java
@@ -0,0 +1,13 @@
+package engine;
+
+
+// Has the ability to read out *TMX-Files and build a Map out of them
+public class Map {
+	
+	public Map(){
+		
+	}
+	
+	
+
+}
---
+++ src/engine/XMLReader.java
@@ -0,0 +1,34 @@
+package engine;
+
+import java.io.File;
+import org.w3c.dom.Document;
+import org.w3c.dom.*;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
+public class XMLReader {
+	
+	private DocumentBuilder docBuilder;
+	
+    public XMLReader() {
+    	try {
+    		DocumentBuilderFactory docBuilderFactory = DocumentBuilderFactory.newInstance();
+            this.docBuilder = docBuilderFactory.newDocumentBuilder();
+        } catch (Exception err) {
+        	err.printStackTrace();
+        }
+    }
+    
+    public Document getDocument(String target){
+    	try {
+    		Document doc = docBuilder.parse (new File(target)); // normalize text representation
+            doc.getDocumentElement ().normalize ();
+            return doc;
+    	} catch(Exception e){
+    		e.printStackTrace();
+    	}
+    	//TODO better fallback solution
+    	return null;	
+    }
+}
