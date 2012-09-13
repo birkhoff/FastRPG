636c56c25aae6cb45d06acb79dcec176608ab458
Alexander Schneider <wegi.pwnz@googlemail.com>
2012-09-13 16:19:24 +0200

    Pre-Maprework

---
+++ src/engine/Objectgroup.java
@@ -0,0 +1,25 @@
+package engine;
+
+public class Objectgroup {
+	
+	private Entity[] objects;
+	private String name;
+	
+	public Objectgroup(Entity[] obj, String name){
+		this.objects = obj;
+		this.name = name;
+	}
+	
+	public Entity[] getObjetcs(){
+		return this.objects;
+	}
+	
+	public int getObjectCount(){
+		return this.objects.length;
+	}
+	
+	public String getName(){
+		return name;
+	}
+
+}
