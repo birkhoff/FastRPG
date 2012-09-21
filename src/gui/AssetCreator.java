df53f8fa9f53a490c26ef71afe897f7b591f4381
n2o <cmeter@googlemail.com>
2012-09-21 16:44:17 +0200

    New Maps with objects

---
+++ src/gui/AssetCreator.java
@@ -0,0 +1,16 @@
+/**
+ * Creates objects, mobs, npcs, ...
+ */
+
+package gui;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AssetCreator {
+	List Mobs = new LinkedList();
+	
+	public void createEnemy(String name, float x, float y) {
+		
+	}
+}
